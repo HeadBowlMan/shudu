2baccd1c218242b2c791e8ce863dc9699c6be226
bruce_reboot <861419537@qq.com>
2023-06-25 12:13:44 +0800

    添加数独类

--- shudu/sudoku.h
+++ shudu/sudoku.h
@@ -6,24 +6,24 @@ enum DIFFICULTY{EASY, MIDDLE, HARD};
 class Sudoku
 {
 private:
-	int n_finale = 0;
+	int final_num = 0;
 	char* game_path = nullptr;
-	int n_game = 0;
+	int game_num = 0;
 	int difficulty = EASY;
-	int n_blank = 20;
-	bool unique = false;
+	int blank_num = 20;
+	bool need_unique = false;
 public:
-	Sudoku(int n_final, char *game_path, int n_game, int difficulty, int n_blank, bool unique) {
-		this->n_finale = n_final;
+	Sudoku(int final_num, char *game_path, int game_num, int difficulty, int blank_num, bool need_unique) {
+		this->final_num = final_num;
 		if (game_path != nullptr) {
-			int len = strlen(game_path);
+			long long len = strlen(game_path);
 			this->game_path = new char[len + 1];
 			strncpy_s(this->game_path, len + 1, game_path, len);
 		}
-		this->n_game = n_game;
+		this->game_num = game_num;
 		this->difficulty = difficulty;
-		this->n_blank = n_blank;
-		this->unique = unique;
+		this->blank_num = blank_num;
+		this->need_unique = need_unique;
 	}
 	~Sudoku() {
 		if (game_path != nullptr) {
